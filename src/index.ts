4897a7581885a3b5b58d213b5af40c24f6b9c3a2
Saksham Bhatt <saksham@nordfinance.io>
2023-02-16 12:46:23 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,61 @@
+import express from "express";
+import cors from "cors";
+import dayjs from "dayjs";
+
+const app = express();
+
+const originWhitelist = [
+  "http://localhost:5173",
+  "https://dev-testnet-nordl.netlify.app/",
+];
+
+const corsOptions = {
+  origin: originWhitelist,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello Express app!");
+});
+
+app.get("/say-hello", (req, res) => {
+  res.status(200).json({ success: true, message: "hello" });
+});
+
+app.post("/login", (req, res) => {
+  if (req.body?.username !== undefined && req.body?.password !== undefined) {
+    if (req.body.username === "test" && req.body.password === "test123") {
+      const userAndTimeStamp = `user:test serverLoginTime: ${new Date().toString()}`;
+
+      res
+        .status(201)
+        .cookie("accessToken", JSON.stringify(userAndTimeStamp), {
+          domain: "localhost",
+          path: "/",
+          secure: false,
+          httpOnly: true,
+          sameSite: "lax",
+          expires: dayjs().add(1, "hours").toDate(),
+          encode: String,
+        })
+        .json({ success: true, message: "test user logged in!" });
+    } else {
+      res
+        .status(401)
+        .json({ success: false, message: "incorrect credentials" });
+    }
+  } else {
+    res
+      .status(400)
+      .json({ success: false, message: "username or password not found" });
+  }
+});
+
+app.listen(8080, () => {
+  console.log("server started on port 8080");
+});
